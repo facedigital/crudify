2d83fa1669c20b4e77a4c47ccec53646373c112a
Luciano Braga <lucianobragaweb@gmail.com>
2023-03-10 14:37:45 -0300

    commit inicial

---
+++ src/Providers/CrudGeneratorServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Facedigital\FaceGen\Providers;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class FaceGenServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    public function provides()
+    {
+
+    }
+
+}
