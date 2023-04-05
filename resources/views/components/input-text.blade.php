8db03ecf7f4fc5ab43f57890d5994f23b7091763
Luciano Braga <lucianobragaweb@gmail.com>
2023-04-05 16:36:10 -0300

    fix

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}:</label>
-    <input type="text" class="form-control" name="{{ $name }}" required />
+    <input type="text" class="form-control" name="{{ $name }}" value="{{ $value }}" />
 </div>
 
 
