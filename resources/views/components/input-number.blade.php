c907b1cc51b583df2a74f5f81443b1ef3765f304
Luciano Braga <lucianobragaweb@gmail.com>
2023-04-06 14:16:03 -0300

    feat: melhora layout das views

--- resources/views/components/input-number.blade.php
+++ resources/views/components/input-number.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}:</label>
-    <input type="number" class="form-control" name="{{ $name }}" value="{{ $value }}" />
+    <input type="number" class="form-control" name="{{ $name }}" value="{{ $value }}" class="block rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300" />
 </div>
